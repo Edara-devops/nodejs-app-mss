3def529e2f76b51d12bcac88589e27437fed5bf0
Edara72 <110002592+Edara72@users.noreply.github.com>
2022-09-08 00:24:30 +0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://65.0.76.172:9000/',
+  serverUrl: 'http://13.233.49.12:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': '9bcdd15299583c728cf53837f2ab21fcb36c5aa4',
+	    'sonar.login': '9a89df72cc1251a68d6effc3369113f9701324aa',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
